6aec7489ab546d2b0a901bd1aeecb21929d36309
rodosabc <rodosabc@gmail.com>
2016-03-28 16:45:02 +0300

    Product model added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Product.delete_all
+#...
+Product.create!(title: 'Диван "Превосходство"',
+                description:"Серия 2 Сезон 24",
+                image_url:'homer3.jpg',
+                price: 1488.01)
+#...
+Product.create!(title: 'Диван "Мужское Счастье"',
+                description:"Серия 3 Сезон 24",
+                image_url:'homer1.jpg',
+                price: 228.95)
+#...
+Product.create!(title: 'Диван "Блаженство"',
+                description:"Серия 13 Сезон 24",
+                image_url:'homer2.jpg',
+                price: 300.9)
+#...
