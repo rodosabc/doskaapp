10515fc5ed34849a034d62f98c8701fd822c00c6
rodosabc <rodosabc@gmail.com>
2016-04-15 02:43:04 +0300

    Мелкие изменения

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160412094300) do
+ActiveRecord::Schema.define(version: 20160414231848) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,11 +20,15 @@ ActiveRecord::Schema.define(version: 20160412094300) do
     t.string   "title"
     t.text     "text"
     t.date     "date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.string   "image_url"
     t.string   "author"
     t.string   "theme"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "carts", force: :cascade do |t|
