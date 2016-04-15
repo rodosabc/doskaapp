e988359e2b6411a3078442134926767b4e9221d8
rodosabc <rodosabc@gmail.com>
2016-04-15 11:15:55 +0300

    Пытаюсь залить скрипт

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -2,5 +2,25 @@ $(document).ready(function(){
     $('.slider').glide({
         autoplay: 4000
     });
+    $('input[value="Добавить"]').click(function(){
+            var form = $(this).parent();
+            var req = form.serialize();
+
+                $().ajax({
+                        type: 'POST',
+                    url: form.attr('action'),
+                    data: req,
+                    success: function(data) {
+                       var count = $('#count').text().empty();
+                       count = +count + 1;$('#count').append(count);
+                    },
+                error:  function(xhr, str){
+                        alert('Возникла ошибка: ' + xhr.responseCode);
+                    }
+            })
+
+                return false;
+        })
 })
+
 
