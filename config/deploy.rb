bec2e40684c429d5c61187c4760fa65739973009
rodosabc <rodosabc@gmail.com>
2016-05-02 19:30:37 +0300

    Я не знаю, что я делаю

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,54 @@
+# -*- encoding : utf-8 -*-
+# _your_login_ - Поменять на ваш логин в панели управления
+# _your_project_ - Поменять на имя вашего проекта
+# _your_server_ - Поменять на имя вашего сервера (Указано на вкладке "FTP и SSH" вашей панели управления)
+# set :repository - Установить расположение вашего репозитория
+# У вас должна быть настроена авторизация ssh по сертификатам
+
+set :application, 'doskaapp'
+set :repository,  "ssh://hosting_usandy@chromium.locum.ru/home/hosting_usandy/doskaapp.git"
+
+dpath = "/home/hosting_usandyprojects/svoyadoska-test"
+
+set :user, "hosting_usandy"
+set :use_sudo, false
+set :deploy_to, dpath
+
+set :scm, :git
+
+role :web, "chromium.locum.ru"                          # Your HTTP server, Apache/etc
+role :app, "chromium.locum.ru"                          # This may be the same as your `Web` server
+role :db,  "chromium.locum.ru", :primary => true # This is where Rails migrations will run
+
+after "deploy:update_code", :copy_database_config
+
+task :copy_database_config, roles => :app do
+  db_config = "#{shared_path}/database.yml"
+  run "cp #{db_config} #{release_path}/config/database.yml"
+end
+
+set :unicorn_rails, "/var/lib/gems/1.8/bin/unicorn_rails"
+set :unicorn_conf, "/etc/unicorn/_your_project_._your_login_.rb"
+set :unicorn_pid, "/var/run/unicorn/_your_project_._your_login_.pid"
+
+# - for unicorn - #
+namespace :deploy do
+  desc "Start application"
+  task :start, :roles => :app do
+    run "#{unicorn_rails} -Dc #{unicorn_conf}"
+  end
+
+  desc "Stop application"
+  task :stop, :roles => :app do
+    run "[ -f #{unicorn_pid} ] && kill -QUIT `cat #{unicorn_pid}`"
+  end
+
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "[ -f #{unicorn_pid} ] && kill -USR2 `cat #{unicorn_pid}` || #{unicorn_rails} -Dc #{unicorn_conf}"
+  end
+end
+=begin
 # encoding: utf-8
 # config valid only for Capistrano 3
 lock '3.5.0'
@@ -87,3 +138,4 @@ namespace :deploy do
     end
   end
 end
+=end
